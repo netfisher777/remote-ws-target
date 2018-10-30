9d7155d96fab08f9087294da8d023dee79d037ac
netfisher777 <netfisher777@gmail.com>
2018-10-30 21:31:40 +0300

    fix something weird

--- wf-consul-registrar/src/main/java/com/magicvalleyworks/remotewstarget/wfconsulregistrar/startup/ConsulRegistrarStartupBean.java
+++ wf-consul-registrar/src/main/java/com/magicvalleyworks/remotewstarget/wfconsulregistrar/startup/ConsulRegistrarStartupBean.java
@@ -1,16 +1,23 @@
 package com.magicvalleyworks.remotewstarget.wfconsulregistrar.startup;
 
 import com.magicvalleyworks.remotewstarget.wfconsulregistrar.api.ConsulRegistrar;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Startup
 @Singleton
 public class ConsulRegistrarStartupBean {
+    private static final Logger logger = LoggerFactory.getLogger(ConsulRegistrarStartupBean.class);
+
     @Inject
     private ConsulRegistrar consulRegistrar;
 
@@ -20,6 +27,19 @@ public class ConsulRegistrarStartupBean {
     }
 
     private void registerServices() {
+
+        try (InputStream wsRegConfInputStream = ClassLoader.class.getResourceAsStream("ws-reg-conf.json")) {
+            if (wsRegConfInputStream != null) {
+                String wsRegConfString = IOUtils.toString(wsRegConfInputStream);
+                logger.info(String.format("Content of the wsRegConfString = %s", wsRegConfString));
+            } else {
+                logger.info("wsRegConfInputStream is null");
+            }
+        } catch(IOException ex) {
+            logger.error("", ex);
+        }
+
+
         consulRegistrar.registerWebService("SimpleWebService", "rwst/SimpleWebService?wsdl");
     }
 
